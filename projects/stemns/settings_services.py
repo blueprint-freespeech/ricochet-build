dc1fa8a4b18468440e589c685eafb266211595dc
JeremyRand <jeremyrand@airmail.cc>
2019-12-18 13:50:27 +0000

    Bug 30558: Optionally enable Namecoin
    Namecoin will be enabled if all the following are true:
    
    * rbm var/namecoin is set to 1 at build time
        * This is true for nightly on linux only
    * env var TOR_ENABLE_NAMECOIN=1 is set at run-time

---
+++ projects/stemns/settings_services.py
@@ -0,0 +1,4 @@
+_service_to_command = {
+    "bit.onion": ['TorBrowser/ncprop279/ncprop279', '--conf=TorBrowser/Data/ncprop279/ncprop279.conf'],
+    "bit": ['TorBrowser/ncprop279/ncprop279', '--conf=TorBrowser/Data/ncprop279/ncprop279.conf'],
+}
